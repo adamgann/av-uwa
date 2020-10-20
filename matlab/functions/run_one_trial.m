5b55280cb2d39073c4391538e17b33eee9e45867
Adam Gannon <adammgannon@gmail.com>
2020-10-20 13:59:38 -0400

    Added switch to generate Gold sequence if Comm toolbox available, otherwise load .mat file.

--- matlab/functions/run_one_trial.m
+++ matlab/functions/run_one_trial.m
@@ -1,5 +1,5 @@
 function [ berVec, sinrVec, chanFit ] = run_one_trial( Params , hmatUser, hmatInterf, interfOn )
-% Get one datapoint of the simulation. 
+% Get one datapoint of the simulation.
 %
 % Adam Gannon, SUNY Buffalo, 2018
 
@@ -9,17 +9,18 @@ persistent masterCodeSeq gT;
 
 if isempty(masterCodeSeq)
     % In this sim, the signatures are static Gold sequences, only the length is
-    % varied. 
-    if (0)
+    % varied. Generate from comm toolbox, if exists. Otherwise load .mat 
+    if (exist('comm.GoldSequence'))
         hGold = comm.GoldSequence;
         hGold.SamplesPerFrame = 64;
         masterCodeSeq = step(hGold);
         masterCodeSeq(masterCodeSeq == 0) = -1;
+    else
+      load masterCodeSeq.mat
     end
-    load masterCodeSeq.mat
 end
 
-if isempty(gT) 
+if isempty(gT)
     if (exist('rcosdesign'))
         g_T = rcosdesign(Params.alpha,6,Params.sps,'sqrt');
     else
@@ -56,7 +57,7 @@ chanGainStore = zeros(1,numSim);
 
 
 % Iterator for channels.
-iChanVec = 1; 
+iChanVec = 1;
 
 
 
@@ -67,21 +68,21 @@ while iRun<(numSim+1)
     if (numSim>1)
         iRun
     end
-    
+
     % Set duration and snapshots dynamically based on the target packet
     % duration and the symbol duration
-    symbolDuration = Params.L*Params.Tc;   
+    symbolDuration = Params.L*Params.Tc;
     if (Params.maxSnapshots)
-       
+
         Params.N = floor(Params.targetPacketDuration / symbolDuration);                          % Adjust packet duration to coherence time
         packetDuration = (Params.N*Params.L*Params.Tc);                                   % Actual packet duration
-        Params.numSnapshots = Params.N;                                            %Use all packet bits 
+        Params.numSnapshots = Params.N;                                            %Use all packet bits
     end
-        
+
     % Number of chunks
     symbolsPerDt = floor(Params.dt/symbolDuration);
     numDt = ceil(Params.N/symbolsPerDt);
-    
+
     % Primary (Interferers)
     sigInterf=(1/sqrt(Params.L)).*sign(randn(Params.L,Params.K));
     bitsInterf=sign(randn(Params.K,Params.N));
@@ -89,11 +90,11 @@ while iRun<(numSim+1)
     % Secondary (User of Interest)
     sigUser = masterCodeSeq(1:Params.L)./sqrt(Params.L);
     bitsUser=sign(randn(1,Params.N));  % Data bits
-    
+
     %% Create the TX signal vector
-    
+
     if (Params.dynamicChannels)
-        
+
         % Get the TX signal over a chunk of the channel matrix.
         hs_mat = hmatUser(:,iChanVec:iChanVec+numDt-1);
         sigVec = get_signal_dynamic_chan(sigUser, bitsUser, amplUser, hs_mat, Params, numDt, symbolsPerDt);
@@ -105,14 +106,14 @@ while iRun<(numSim+1)
         hs_mat = hmatUser(:,iChanVec);
         sigVec = get_signal_static_chan(sigUser, bitsUser, amplUser, hs_mat, Params);
         iChanVec = iChanVec + numDt;
-    
+
     end
 
-    
-    %% Channel Simulation
-    
 
-    % Generate interference vector 
+    %% Channel Simulation
+
+
+    % Generate interference vector
     interfVec = zeros(size(sigVec));
     for kk=1:K
         if (interfOn)
@@ -120,21 +121,21 @@ while iRun<(numSim+1)
         end
     end
 
-    % Generate noise 
-    if (Params.genColoredNoise) 
+    % Generate noise
+    if (Params.genColoredNoise)
         Noise=get_colored_noise(Params.L,M,Params.N+10,Params);
         noiseVec = reshape(Noise,[],1);
         noiseVec = noiseVec(1:length(sigVec));
     else
-        Noise=(randn(L_M,Params.N)+1i*randn(L_M,Params.N))/sqrt(2);  
-        noiseVec = (randn(size(sigVec))+1i*randn(size(sigVec)))/sqrt(2);        
+        Noise=(randn(L_M,Params.N)+1i*randn(L_M,Params.N))/sqrt(2);
+        noiseVec = (randn(size(sigVec))+1i*randn(size(sigVec)))/sqrt(2);
     end
-    
-    % Add noise and interference to signal vector
-    disturbanceVec = interfVec + noiseVec; 
-    rxVec = sigVec + disturbanceVec; 
 
-    % Pulse-matched filtering 
+    % Add noise and interference to signal vector
+    disturbanceVec = interfVec + noiseVec;
+    rxVec = sigVec + disturbanceVec;
+
+    % Pulse-matched filtering
     symFilt = conv(sigVec, g_T);
     interfFilt = conv(interfVec, g_T);
     rxFilt = conv(rxVec, g_T);
@@ -152,19 +153,19 @@ while iRun<(numSim+1)
     interfMat = vec_to_multipath_mat(interfSampled,Params,Params.Mrec);
     rxMat = vec_to_multipath_mat(rxSampled,Params,Params.Mrec);
 
-    
+
     % Calculate RN matrix
     ySamples = rxMat(:,1:Params.numSnapshots);                                      % Limit the number of snapshots used to calculate R
     R_Rx=(ySamples*ySamples')/size(ySamples,2);                          % Calculate the sample-limited R
 
 
-    
-    %% Receive signal and estimate channel
-    
 
-    % Channel-processed signature matrix 
+    %% Receive signal and estimate channel
+
+
+    % Channel-processed signature matrix
     SS_mat = toeplitz([sigUser;zeros(Params.Mrec-1,1)],[sigUser(1),zeros(1,Params.Mrec-1)]);        %Multipath-processed signature matrix
-    
+
     % Estimate the channel using a fixed number of known bits
     if (Params.estimateChan)
         nTrain = 100; % Keep this constant for all code lengths
@@ -174,11 +175,11 @@ while iRun<(numSim+1)
         hs_vec = hestVec;
     else
         % Use the first channel instance for the entire packet
-        hs_vec = hs_mat(1:Params.Mrec,1); 
-    end  
-    
+        hs_vec = hs_mat(1:Params.Mrec,1);
+    end
+
     chanGainStore(iRun) = 10*log10(hs_vec'*hs_vec);
-    
+
     if (Params.plotChanEst)
         figure;
         hold on;
@@ -187,33 +188,33 @@ while iRun<(numSim+1)
         plot(real(hestVec))
         legend('Start','End','Est')
     end
-    
-    
+
+
     if (Params.plotPsd)
         Nfft = 1024;
         fs=5e6;
-        
+
         y_vec = reshape(Noise,numel(Noise),1);  %Reshape into vector
         y_cut = y_vec(1:Nfft*floor(length(y_vec)/Nfft));
         y_split=reshape(y_cut,Nfft,[]);
         Y_split = fft(y_split,Nfft);
-        
+
         Pk = mean( abs(Y_split).^2, 2) / Nfft;
         Pk_wattsHz = Pk / fs;
         Pk_dbHz = 10*log10(Pk_wattsHz);
         Pk_ss = Pk_dbHz(1:floor(end/2));
-        
-        
+
+
         y_vec = reshape(Noise,numel(Noise),1);  %Reshape into vector
         y_cut = y_vec(1:Nfft*floor(length(y_vec)/Nfft));
         y_split=reshape(y_cut,Nfft,[]);
         Y_split = fft(y_split,Nfft);
-        
+
         Pk = mean( abs(Y_split).^2, 2) / Nfft;
         Pk_wattsHz = Pk / fs;
         Pk_dbHz = 10*log10(Pk_wattsHz);
         Pk_ds = Pk_dbHz(1:floor(end/2));
-        
+
         deltaf = fs/Nfft;
         fx = (0:Nfft/2-1)*deltaf;
 
@@ -221,13 +222,13 @@ while iRun<(numSim+1)
         y_split=reshape(sigMat(:,1:Nfft*floor(Params.N/Nfft)),Nfft,[]);
 
         Y_split = fft(y_split,Nfft);
-        
+
         Sk = mean( abs(Y_split).^2, 2) / Nfft;
         Sk_wattsHz = Sk / fs;
         Sk_dbHz = 10*log10(Sk_wattsHz);
-        Sk_ss = Sk_dbHz(1:floor(end/2));  
-        
-                
+        Sk_ss = Sk_dbHz(1:floor(end/2));
+
+
         figure;
         plot(fx,Pk_ss,'r','LineWidth', 2)
         hold on
@@ -235,8 +236,8 @@ while iRun<(numSim+1)
         plot(fx,Sk_ss)
         legend('Vector','CT','UOI')
         title('PSD of Interference Plus Noise')
-        
-        
+
+
         return
     end
 
@@ -252,9 +253,9 @@ while iRun<(numSim+1)
     % MaxSINR, built with SMI autocorrelation matrix
     w_sinr = (R_Rx\w_mf)/(w_mf'*(R_Rx\w_mf));
 
-    
-  
-    % Calc AV filter 
+
+
+    % Calc AV filter
     if (Params.skipAvCalc)
         w_av = zeros(size(w_sinr));
     else
@@ -277,26 +278,26 @@ while iRun<(numSim+1)
                 ME.message
                 return
             end
-            
+
         end
     end
-    
 
-    
+
+
     % Now that convergence has been tested, for convergence
     % both filters must be normalized to get proper bit energy
     w_av = w_av./(norm(SS_mat*hs_vec).^2);
-    w_sinr = w_sinr./(norm(SS_mat*hs_vec).^2) ;  
+    w_sinr = w_sinr./(norm(SS_mat*hs_vec).^2) ;
 
-    
+
     %% Bit and SINR Recovery
 
     % Discard the last few bits of the packet for calculating BER. This is
-    % to account for responses of filters and other simulation effects 
+    % to account for responses of filters and other simulation effects
     nBitsToCut = 5;
     bitsUser = bitsUser(1:end-nBitsToCut);
 
-    
+
     % Recover matched filter bits
     rxMf = (w_mf'*rxMat)/amplUser;
     rxMf = rxMf(1:end-nBitsToCut);
@@ -304,7 +305,7 @@ while iRun<(numSim+1)
     bitRecMf=sign(real(rxMf));
     errvecMf = bitsUser ~= bitRecMf;
     berMf(iRun) = nnz(errvecMf)/length(errvecMf);
-    
+
 
 
     % Recover maxSINR bits
@@ -320,31 +321,31 @@ while iRun<(numSim+1)
     rxAv = (w_av'*rxMat)/amplUser;
     rxAv = rxAv(1:end-nBitsToCut);
     bitRecAv=sign(real(rxAv));
-    
+
 
     errvecAv = bitsUser ~= bitRecAv;
     berAv(iRun)=nnz(errvecAv)/length(errvecAv);
-    
+
 
     evmMf = sqrt(mean(abs(rxMf-bitsUser).^2));
     evmMvdr = sqrt(mean(abs(rxMaxsinr-bitsUser).^2));
     evmAv = sqrt(mean(abs(rxAv-bitsUser).^2));
-    
+
     sinrMf(iRun) = 10*log10(1/evmMf.^2);
     sinrMvdr(iRun) = 10*log10(1/evmMvdr.^2);
     sinrAv(iRun) = 10*log10(1/evmAv.^2);
-    
-    % Prefiltering SINR 
+
+    % Prefiltering SINR
     sigPower = sum(abs(sigVec).^2)/length(sigVec);
     distPower = sum(abs(disturbanceVec).^2)/length(disturbanceVec);
     sinrPrefilt(iRun) = 10*log10(sigPower/distPower);
-    
-    
+
+
 
 
     % Plot scatterplots
     if (Params.plotScatter)
-        
+
         if exist('scatterplot')
             scatterplot(rxMf(1:100))
             title('MF')
@@ -362,12 +363,12 @@ while iRun<(numSim+1)
             figure;scatter(real(rxAv(1:100)),imag(rxAv(1:100)))
             title('AV')
 
-            
+
         end
 
     end
 
-    
+
 iRun = iRun + 1;   % Increase index
 end
 
@@ -397,7 +398,7 @@ sinrVec = containers.Map(keySet,sinrSet);
 % Get the polynomial fit
 chanFit = polyfit(chanGainStore,sinrAv,1);
 
-% Calculate the 95% confidence interval 
+% Calculate the 95% confidence interval
 if exist('polyfit')
     [chanPoly,S,mu] = polyfit(chanGainStore,sinrAv,1);
     [fitLine,chanDelta] = polyval(chanPoly,chanGainStore,S,mu);
@@ -419,5 +420,3 @@ if (0)
 end
 
 return
-
-
